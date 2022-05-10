2ba3056c654c7b16dae623efaa4b90892b3f704a
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-09 21:37:37 -0300

    Aplicando anotações de configuração na classe BookController

--- src/main/java/br/com/library/api/controller/BookController.java
+++ src/main/java/br/com/library/api/controller/BookController.java
@@ -1,4 +1,9 @@
 package br.com.library.api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/books")
 public class BookController {
 }
