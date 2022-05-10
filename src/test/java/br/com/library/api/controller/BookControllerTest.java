f06e910677ebe84ca33db3fd5557c3d0f948f5a3
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-09 21:05:32 -0300

    Criação classe BookControllerTest

---
+++ src/test/java/br/com/library/api/controller/BookControllerTest.java
@@ -0,0 +1,9 @@
+package br.com.library.api.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class BookControllerTest {
+}
