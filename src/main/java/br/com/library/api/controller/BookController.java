b8189ce28a674735783b7f873f9b26157fff0a81
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-09 22:45:50 -0300

    Implementação da função create na classe BookController

--- src/main/java/br/com/library/api/controller/BookController.java
+++ src/main/java/br/com/library/api/controller/BookController.java
@@ -1,19 +1,24 @@
 package br.com.library.api.controller;
 
 import br.com.library.api.dto.BookDTO;
+import br.com.library.api.model.Book;
+import br.com.library.api.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/books")
 public class BookController {
 
+    @Autowired
+    private BookService bookService;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDTO create() {
-        return new BookDTO(1L, "Clean Code", "Robert Cecil Martin", "121321");
+    public BookDTO create(@RequestBody BookDTO dto) {
+        Book entity = Book.builder().title(dto.getTitle()).author(dto.getAuthor()).isbn(dto.getIsbn()).build();
+        entity = bookService.save(entity);
+        return BookDTO.builder().id(entity.getId()).author(entity.getAuthor()).title(entity.getTitle()).isbn(entity.getIsbn()).build();
     }
 }
