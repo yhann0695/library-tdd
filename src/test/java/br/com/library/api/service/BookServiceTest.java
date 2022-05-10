f2286cc1eae6cadeb2ee99e6c43b98e26e1af9b3
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-10 00:24:11 -0300

    Implementação da função saveBookTest para teste de salvar book.

--- src/test/java/br/com/library/api/service/BookServiceTest.java
+++ src/test/java/br/com/library/api/service/BookServiceTest.java
@@ -1,10 +1,20 @@
 package br.com.library.api.service;
 
+import br.com.library.api.dto.BookDTO;
+import br.com.library.api.model.Book;
+import br.com.library.api.repository.BookRepository;
+import br.com.library.api.service.impl.BookServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("Test")
@@ -12,9 +22,29 @@ public class BookServiceTest {
 
     BookService bookService;
 
+    @MockBean
+    BookRepository bookRepository;
+
+    @BeforeEach
+    void setUp() {
+        this.bookService = new BookServiceImpl(bookRepository);
+    }
+
     @Test
     @DisplayName("Must save a book")
     void saveBookTest() {
+        Book book = Book.builder().title("Clean Code").author("Robert Cecil Martin").isbn("121321").build();
+
+        Mockito.when( bookRepository.save(book))
+                .thenReturn(Book.builder().id(1L).title("Clean Code").author("Robert Cecil Martin").isbn("121321").build());
+
+        Book savedBook = bookService.save(book);
+
+        assertThat(savedBook.getId()).isNotNull();
+        assertThat(savedBook.getTitle()).isEqualTo("Clean Code");
+        assertThat(savedBook.getAuthor()).isEqualTo("Robert Cecil Martin");
+        assertThat(savedBook.getIsbn()).isEqualTo("121321");
+
 
     }
 }
