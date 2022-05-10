b258d6b35e7621d2e1128b1eb97154ece53f5c1e
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-09 21:52:42 -0300

    Implementação da função create

--- src/main/java/br/com/library/api/controller/BookController.java
+++ src/main/java/br/com/library/api/controller/BookController.java
@@ -1,8 +1,10 @@
 package br.com.library.api.controller;
 
 import br.com.library.api.dto.BookDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create() {
-        return null;
+        return new BookDTO(1L, "Clean Code", "Robert Cecil Martin", "121321");
     }
 }
