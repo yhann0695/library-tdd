2a0db195fb0ab9c4552112043f1ec3cc2945735e
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-09 22:44:40 -0300

    Criação das propriedades e anotações da classe Book

--- src/main/java/br/com/library/api/model/Book.java
+++ src/main/java/br/com/library/api/model/Book.java
@@ -1,4 +1,18 @@
 package br.com.library.api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Book {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
 }
