74585c4e25cf6f1080766275e9742f8df13127f7
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-09 21:26:52 -0300

    CriaEscrita de teste para criação de livro com sucesso

--- src/test/java/br/com/library/api/controller/BookControllerTest.java
+++ src/test/java/br/com/library/api/controller/BookControllerTest.java
@@ -1,15 +1,48 @@
 package br.com.library.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("Test")
 @WebMvcTest
 @AutoConfigureMockMvc
 public class BookControllerTest {
+
+    private static String BOOK_API = "/api/books";
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Successfully create a book")
+    void testCreateBook() throws Exception {
+        String json = new ObjectMapper().writeValueAsString(null);
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content("");
+
+        mockMvc
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("id").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("title").value("Clean Code"))
+                .andExpect(MockMvcResultMatchers.jsonPath("author").value("Robert Cecil Martin"))
+                .andExpect(MockMvcResultMatchers.jsonPath("isbn").value("121321"));
+    }
 }
