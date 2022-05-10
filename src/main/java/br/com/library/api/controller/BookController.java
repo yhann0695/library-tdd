daf5a571f4abad54c4eb304b3368e7170bbc6d6a
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-09 21:41:57 -0300

    Criação da função de crcriar Books.

--- src/main/java/br/com/library/api/controller/BookController.java
+++ src/main/java/br/com/library/api/controller/BookController.java
@@ -1,9 +1,16 @@
 package br.com.library.api.controller;
 
+import br.com.library.api.dto.BookDTO;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/books")
 public class BookController {
+
+    @PostMapping
+    public BookDTO create() {
+        return null;
+    }
 }
