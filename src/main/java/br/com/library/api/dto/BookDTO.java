07735ca985db58dd519c4843a0796c87c95f2e71
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-09 21:51:40 -0300

    Criação de construtores e tirando a anotação de Setter.

--- src/main/java/br/com/library/api/dto/BookDTO.java
+++ src/main/java/br/com/library/api/dto/BookDTO.java
@@ -1,10 +1,13 @@
 package br.com.library.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookDTO {
 
     private Long id;
