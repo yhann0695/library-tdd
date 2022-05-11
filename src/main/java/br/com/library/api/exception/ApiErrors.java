511f747b2f32854917581e37cd13f146e8bd546d
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-10 21:53:47 -0300

    Classe para retorno das mensagens de erros coletada pela Exception

--- src/main/java/br/com/library/api/exception/ApiErrors.java
+++ src/main/java/br/com/library/api/exception/ApiErrors.java
@@ -1,4 +1,20 @@
 package br.com.library.api.exception;
 
+import org.springframework.validation.BindingResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ApiErrors {
+
+    private List<String> errors;
+
+    public ApiErrors(BindingResult bindingResult) {
+        this.errors = new ArrayList<>();
+        bindingResult.getAllErrors().forEach(error -> this.errors.add(error.getDefaultMessage()));
+    }
+
+    public List<String> getErrors() {
+        return this.errors;
+    }
 }
