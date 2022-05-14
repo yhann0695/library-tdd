fafa94dd4e9502e5a38301e2e9898e1035a529ef
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-14 01:28:14 -0300

    Adição de anotações na classe de teste

--- src/test/java/br/com/library/api/model/BookRepositoryTest.java
+++ src/test/java/br/com/library/api/model/BookRepositoryTest.java
@@ -1,4 +1,12 @@
 package br.com.library.api.model;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
 public class BookRepositoryTest {
 }
