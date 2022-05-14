0c5e67e3281d860257cdcf98d6a240733a7ea38c
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-14 01:51:24 -0300

    Criação da função de teste para ISBN existente

--- src/test/java/br/com/library/api/model/BookRepositoryTest.java
+++ src/test/java/br/com/library/api/model/BookRepositoryTest.java
@@ -1,7 +1,13 @@
 package br.com.library.api.model;
 
+import br.com.library.api.repository.BookRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -9,4 +15,20 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ActiveProfiles("test")
 @DataJpaTest
 public class BookRepositoryTest {
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    @DisplayName("should return true if the ISBN already exists")
+    void testReturnTrueWhenIsbnExists() {
+        String isbn = "123321";
+
+       boolean exists = bookRepository.existsByIsbn(isbn);
+
+        Assertions.assertTrue(exists);
+    }
 }
