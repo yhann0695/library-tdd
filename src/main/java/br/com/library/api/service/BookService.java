b52d5df9acf6331ce2734a0b02a4b940afb74791
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-14 12:38:50 -0300

    Criação de função para pegar ID do livro

--- src/main/java/br/com/library/api/service/BookService.java
+++ src/main/java/br/com/library/api/service/BookService.java
@@ -2,7 +2,11 @@ package br.com.library.api.service;
 
 import br.com.library.api.model.Book;
 
+import java.util.Optional;
+
 public interface BookService {
 
     Book save(Book entity);
+
+    Optional<Book> getById(Long id);
 }
