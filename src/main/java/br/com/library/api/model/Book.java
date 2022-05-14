c48628ded05023369300d34f17067b2aa0257f5c
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-14 11:46:09 -0300

    Anotações de coluna e entidade no Book

--- src/main/java/br/com/library/api/model/Book.java
+++ src/main/java/br/com/library/api/model/Book.java
@@ -2,14 +2,27 @@ package br.com.library.api.model;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "Book")
 public class Book {
 
+    @Id
+    @Column(name = "BOOK_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "BOOK_TITLE")
     private String title;
+
+    @Column(name = "BOOK_AUTHOR")
     private String author;
+
+    @Column(name = "BOOK_ISBN")
     private String isbn;
 }
