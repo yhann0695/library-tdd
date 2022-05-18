806935cf2befd91e87cd088220fb7de3ff68506d
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-18 11:25:29 -0300

    Ceriação da função de find no controller

--- src/main/java/br/com/library/api/controller/BookController.java
+++ src/main/java/br/com/library/api/controller/BookController.java
@@ -7,6 +7,9 @@ import br.com.library.api.model.Book;
 import br.com.library.api.service.BookService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/api/books")
@@ -56,6 +61,17 @@ public class BookController {
             book = bookService.update(book);
             return modelMapper.map(book, BookDTO.class);
         }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping
+    public Page<BookDTO> find(BookDTO dto, Pageable pageRequest) {
+        Book filter = modelMapper.map(dto, Book.class);
+        Page<Book> result = bookService.find(filter, pageRequest);
+        List<BookDTO> bookDTOList = result.getContent()
+                .stream()
+                .map(entity -> modelMapper.map(entity, BookDTO.class)).collect(Collectors.toList());
+
+        return new PageImpl<BookDTO>(bookDTOList, pageRequest, result.getTotalElements());
 
     }
 
