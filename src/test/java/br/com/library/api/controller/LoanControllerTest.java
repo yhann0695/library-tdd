efde45528c06bd7d2ba6960a4a9486e561cf0a87
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-19 22:44:20 -0300

    Criação classe de teste para LoanControllerTest junto de anotações.

---
+++ src/test/java/br/com/library/api/controller/LoanControllerTest.java
@@ -0,0 +1,16 @@
+package br.com.library.api.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("Test")
+@WebMvcTest(controllers = {LoanController.class})
+@AutoConfigureMockMvc
+public class LoanControllerTest {
+
+
+}
