dd945786b857731ceb14aedf398da490f17d687b
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-19 22:46:35 -0300

    injeção do MockMvc para teste na classe LoanController

--- src/test/java/br/com/library/api/controller/LoanControllerTest.java
+++ src/test/java/br/com/library/api/controller/LoanControllerTest.java
@@ -1,16 +1,22 @@
 package br.com.library.api.controller;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("Test")
 @WebMvcTest(controllers = {LoanController.class})
 @AutoConfigureMockMvc
 public class LoanControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
 
 
 }
