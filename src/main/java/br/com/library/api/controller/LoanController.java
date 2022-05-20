2631d31dc6d24ee2f655259602ed8ff00c3ef110
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-20 07:51:29 -0300

    Criação da função e inicio de implementação POST na RestController Loan

--- src/main/java/br/com/library/api/controller/LoanController.java
+++ src/main/java/br/com/library/api/controller/LoanController.java
@@ -1,4 +1,36 @@
 package br.com.library.api.controller;
 
+import br.com.library.api.dto.LoanDTO;
+import br.com.library.api.model.Book;
+import br.com.library.api.model.Loan;
+import br.com.library.api.service.BookService;
+import br.com.library.api.service.LoanService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/loans")
 public class LoanController {
+
+    @Autowired
+    private LoanService loanService;
+
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long create(@RequestBody LoanDTO dto) {
+        Book book = bookService.getBookByIsbn(dto.getIsbn()).get();
+        Loan entity = Loan.builder().book(book).customer(dto.getCustomer()).loanDate(LocalDate.now()).build();
+        entity = loanService.save(entity);
+        return entity.getId();
+    }
 }
