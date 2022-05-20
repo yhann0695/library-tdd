624d9ffbd47f7084980d1438bc70d2d493d9a9f0
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-19 23:13:19 -0300

    Criação das propriedades na classe Loan

--- src/main/java/br/com/library/api/model/Loan.java
+++ src/main/java/br/com/library/api/model/Loan.java
@@ -5,9 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Loan {
+
+    private Long id;
+    private String customer;
+    private Book book;
+    private LocalDate loanDate;
+    private boolean returned;
 }
