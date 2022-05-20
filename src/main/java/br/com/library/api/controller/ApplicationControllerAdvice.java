4055b85871c63684cfe2f67c80793fb23896bd4e
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-20 14:40:00 -0300

    Nova função para tratar erros de ResponseStatusException

--- src/main/java/br/com/library/api/controller/ApplicationControllerAdvice.java
+++ src/main/java/br/com/library/api/controller/ApplicationControllerAdvice.java
@@ -3,11 +3,13 @@ package br.com.library.api.controller;
 import br.com.library.api.exception.ApiErrors;
 import br.com.library.api.exception.BusinessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -23,5 +25,11 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiErrors handleBusinessException(BusinessException args) {
         return new ApiErrors(args);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiErrors> handleResponseStatusException(ResponseStatusException args) {
+        return new ResponseEntity<>(new ApiErrors(args), args.getStatus());
     }
 }
