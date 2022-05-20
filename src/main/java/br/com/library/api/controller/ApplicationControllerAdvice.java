885942f06510a15a2f191fb7b57f3d8293248c96
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-20 14:30:50 -0300

    Criação do controller advice para controle de erros

---
+++ src/main/java/br/com/library/api/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,7 @@
+package br.com.library.api.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+}
