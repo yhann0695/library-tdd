63b250ad020793b9f9c81bb90d6e80d6e769a03f
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-22 15:11:48 -0300

    Implementação da função de buscar emprestimos no controller

--- src/main/java/br/com/library/api/controller/LoanController.java
+++ src/main/java/br/com/library/api/controller/LoanController.java
@@ -1,6 +1,8 @@
 package br.com.library.api.controller;
 
+import br.com.library.api.dto.BookDTO;
 import br.com.library.api.dto.LoanDTO;
+import br.com.library.api.dto.LoanFilterDTO;
 import br.com.library.api.dto.ReturnedLoadDTO;
 import br.com.library.api.model.Book;
 import br.com.library.api.model.Loan;
@@ -8,11 +10,16 @@ import br.com.library.api.service.BookService;
 import br.com.library.api.service.LoanService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/loans")
@@ -45,5 +52,20 @@ public class LoanController {
         loan.setReturned(dto.getReturned());
 
         loanService.update(loan);
+    }
+
+    @GetMapping
+    public Page<LoanDTO> find(LoanFilterDTO dto, Pageable pageRequest) {
+        Page<Loan> result = loanService.find(dto, pageRequest);
+        List<LoanDTO> loanDTOList = result.getContent()
+                                        .stream()
+                                        .map(entity -> {
+                                            Book book = entity.getBook();
+                                            BookDTO bookDTO = modelMapper.map(book, BookDTO.class);
+                                            LoanDTO loanDTO = modelMapper.map(entity, LoanDTO.class);
+                                            loanDTO.setBook(bookDTO);
+                                            return loanDTO;
+                                        }).collect(Collectors.toList());
+        return new PageImpl<LoanDTO>(loanDTOList, pageRequest, result.getTotalElements());
     }
 }
