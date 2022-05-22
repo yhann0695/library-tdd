43ec7016efc667352c401ecb8824dec3ec1e5f22
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-22 16:46:36 -0300

    Ciração da função findByBookIsbnOrCustomer

--- src/main/java/br/com/library/api/repository/LoanRepository.java
+++ src/main/java/br/com/library/api/repository/LoanRepository.java
@@ -2,6 +2,9 @@ package br.com.library.api.repository;
 
 import br.com.library.api.model.Book;
 import br.com.library.api.model.Loan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +14,6 @@ public interface LoanRepository extends JpaRepository<Loan, Long> {
     @Query(value = "select case when (count(l.id) > 0) then true else false end" +
             " from Loan l where l.book = :book and (l.returned is null or l.returned is false)")
     boolean existsByBookAndNotReturned(@Param("book") Book book);
+
+    Page<Loan> findByBookIsbnOrCustomer(String isbn, String customer, Pageable pageable);
 }
