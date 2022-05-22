d02554f6e06f868b7d8788d7af038de1fabb19ea
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-22 16:45:38 -0300

    Implementação das anotações

--- src/main/java/br/com/library/api/dto/LoanFilterDTO.java
+++ src/main/java/br/com/library/api/dto/LoanFilterDTO.java
@@ -1,8 +1,14 @@
 package br.com.library.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoanFilterDTO {
 
     private String isbn;
