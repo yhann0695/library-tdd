96e286c6f0d518f04be2d3ba933391e3e285ac54
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-21 23:52:15 -0300

    Implementação de QUERY para busca dos livros que foram emprestados e que não foram retornados

--- src/main/java/br/com/library/api/repository/LoanRepository.java
+++ src/main/java/br/com/library/api/repository/LoanRepository.java
@@ -3,8 +3,12 @@ package br.com.library.api.repository;
 import br.com.library.api.model.Book;
 import br.com.library.api.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 
-    boolean existsByBookAndNotReturned(Book book);
+    @Query(value = "select case when (count(l.id) > 0) then true else false end" +
+            " from Loan l where l.book = :book and (l.returned is null or l.returned is false)")
+    boolean existsByBookAndNotReturned(@Param("book") Book book);
 }
