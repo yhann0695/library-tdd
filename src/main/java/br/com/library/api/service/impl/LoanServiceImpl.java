2328ddbc2e7ab0718e6ad91a9e067cc119871685
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-22 11:28:13 -0300

    Implementação da função de update do serviço

--- src/main/java/br/com/library/api/service/impl/LoanServiceImpl.java
+++ src/main/java/br/com/library/api/service/impl/LoanServiceImpl.java
@@ -32,6 +32,6 @@ public class LoanServiceImpl implements LoanService {
 
     @Override
     public Loan update(Loan loan) {
-        return null;
+        return loanRepository.save(loan);
     }
 }
