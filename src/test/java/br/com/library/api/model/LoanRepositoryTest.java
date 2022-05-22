decc8681dd388f0e900dce78fcc3b16cd84cd57a
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-21 23:16:36 -0300

    Criação da classe de teste de integração de LoanRepository

---
+++ src/test/java/br/com/library/api/model/LoanRepositoryTest.java
@@ -0,0 +1,23 @@
+package br.com.library.api.model;
+
+import br.com.library.api.repository.LoanRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
+public class LoanRepositoryTest {
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    LoanRepository loanRepository;
+
+
+}
