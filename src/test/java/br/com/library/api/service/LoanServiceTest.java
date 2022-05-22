b5c1642bc7193169d7a2bc4ec7b7f84645e9b11e
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-21 21:10:34 -0300

    Implementação da da classe LoanServiceTest

---
+++ src/test/java/br/com/library/api/service/LoanServiceTest.java
@@ -0,0 +1,12 @@
+package br.com.library.api.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("Test")
+public class LoanServiceTest {
+
+
+}
