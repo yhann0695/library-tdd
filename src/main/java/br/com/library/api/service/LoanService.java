f19566e961705fe0c6c9fe23109b5d97ba799d4d
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-22 00:59:10 -0300

    Criação e sobrescrita das funções de update e getById

--- src/main/java/br/com/library/api/service/LoanService.java
+++ src/main/java/br/com/library/api/service/LoanService.java
@@ -2,7 +2,14 @@ package br.com.library.api.service;
 
 import br.com.library.api.model.Loan;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LoanService {
 
     public Loan save(Loan loan);
+
+    Optional<Loan> getById(Long id);
+
+    Loan update(Loan loan);
 }
--- src/main/java/br/com/library/api/service/impl/LoanServiceImpl.java
+++ src/main/java/br/com/library/api/service/impl/LoanServiceImpl.java
@@ -6,6 +6,8 @@ import br.com.library.api.repository.LoanRepository;
 import br.com.library.api.service.LoanService;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class LoanServiceImpl implements LoanService {
 
@@ -21,5 +23,15 @@ public class LoanServiceImpl implements LoanService {
             throw new BusinessException("Book already loaned");
         }
         return loanRepository.save(loan);
+    }
+
+    @Override
+    public Optional<Loan> getById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Loan update(Loan loan) {
+        return null;
     }
 }
