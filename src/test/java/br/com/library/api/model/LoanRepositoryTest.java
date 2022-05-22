48d066c4b5917bb3c831854caa5f95ce39f5d554
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-21 23:51:37 -0300

    Implementação da função de teste de integração para livros emprestados que não foram retornados

--- src/test/java/br/com/library/api/model/LoanRepositoryTest.java
+++ src/test/java/br/com/library/api/model/LoanRepositoryTest.java
@@ -1,12 +1,18 @@
 package br.com.library.api.model;
 
 import br.com.library.api.repository.LoanRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -20,4 +26,18 @@ public class LoanRepositoryTest {
     LoanRepository loanRepository;
 
 
+    @Test
+    @DisplayName("should check if there is an unreturned loan for the book")
+    void testExistsByBookAndNotReturned() {
+
+        Book book = Book.builder().title("Clean Code").author("Robert C. Martin").isbn("123").build();
+        entityManager.persist(book);
+
+        Loan loan = Loan.builder().book(book).customer("Joãozin").loanDate(LocalDate.now()).build();
+        entityManager.persist(loan);
+
+        boolean exists = loanRepository.existsByBookAndNotReturned(book);
+
+        assertThat(exists).isTrue();
+    }
 }
