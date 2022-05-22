3e5ca0b56c8246bba05245ce5c74791744d3d305
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-21 23:07:05 -0300

    Criação da função para verificar se existe livro emprestado

--- src/main/java/br/com/library/api/repository/LoanRepository.java
+++ src/main/java/br/com/library/api/repository/LoanRepository.java
@@ -1,7 +1,10 @@
 package br.com.library.api.repository;
 
+import br.com.library.api.model.Book;
 import br.com.library.api.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+    boolean existsByBookAndNotReturned(Book book);
 }
