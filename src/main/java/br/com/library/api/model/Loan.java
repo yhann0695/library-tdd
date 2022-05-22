e198b222d53f77f7899a57a5be9faf2437f18dbd
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-21 23:49:40 -0300

    Colocando anotação de entidade na classe Loan

--- src/main/java/br/com/library/api/model/Loan.java
+++ src/main/java/br/com/library/api/model/Loan.java
@@ -5,17 +5,32 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "Loan")
 public class Loan {
 
+    @Id
+    @Column(name = "LOAN_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "LOAN_CUSTOMER")
     private String customer;
+
+    @JoinColumn(name = "BOOK_ID")
+    @ManyToOne
     private Book book;
+
+    @Column(name = "LOAN_DATE")
     private LocalDate loanDate;
+
+    @Column(name = "LOAN_RETURNED")
     private boolean returned;
 }
