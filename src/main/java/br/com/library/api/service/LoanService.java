0779d44f592cc4d678ba0b306371284cd221bebb
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-22 15:09:22 -0300

    Criação da função de buscar emprestimos

--- src/main/java/br/com/library/api/service/LoanService.java
+++ src/main/java/br/com/library/api/service/LoanService.java
@@ -1,6 +1,9 @@
 package br.com.library.api.service;
 
+import br.com.library.api.dto.LoanFilterDTO;
 import br.com.library.api.model.Loan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +15,6 @@ public interface LoanService {
     Optional<Loan> getById(Long id);
 
     Loan update(Loan loan);
+
+    Page<Loan> find(LoanFilterDTO dto, Pageable pageable);
 }
