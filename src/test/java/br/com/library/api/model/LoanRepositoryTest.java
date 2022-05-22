39e354fb9630033afbb7ae914d6889fa46d01b09
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-22 17:14:52 -0300

    Criação de teste para FindByBookIsbnOrCustomer

--- src/test/java/br/com/library/api/model/LoanRepositoryTest.java
+++ src/test/java/br/com/library/api/model/LoanRepositoryTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -30,14 +32,37 @@ public class LoanRepositoryTest {
     @DisplayName("should check if there is an unreturned loan for the book")
     void testExistsByBookAndNotReturned() {
 
+        Loan loan = createAndPersistLoan();
+        Book book = loan.getBook();
+
+        boolean exists = loanRepository.existsByBookAndNotReturned(book);
+
+        assertThat(exists).isTrue();
+    }
+
+    @Test
+    @DisplayName("must filter loan per isbn or customer")
+    void testFindByBookIsbnOrCustomer() {
+        Loan loan = createAndPersistLoan();
+
+        Page<Loan> result =  loanRepository
+                            .findByBookIsbnOrCustomer("123", "Joãozin", PageRequest.of(0, 10));
+
+        assertThat(result.getContent()).hasSize(1);
+        assertThat(result.getContent()).contains(loan);
+        assertThat(result.getPageable().getPageSize()).isEqualTo(10);
+        assertThat(result.getPageable().getPageNumber()).isEqualTo(0);
+        assertThat(result.getTotalElements()).isEqualTo(1);
+
+    }
+
+    private Loan createAndPersistLoan() {
         Book book = Book.builder().title("Clean Code").author("Robert C. Martin").isbn("123").build();
         entityManager.persist(book);
 
         Loan loan = Loan.builder().book(book).customer("Joãozin").loanDate(LocalDate.now()).build();
         entityManager.persist(loan);
 
-        boolean exists = loanRepository.existsByBookAndNotReturned(book);
-
-        assertThat(exists).isTrue();
+        return loan;
     }
 }
